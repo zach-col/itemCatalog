62680f5313577efd5c0b48917e5bd0a36c4265f8
zach-col <theperfectpicture100@gmail.com>
2017-12-26 15:27:29 -0600

    Create all logout methods and templates

--- application.py
+++ application.py
@@ -1,17 +1,74 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Catalog, CatalogItem
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+
+
 
 @app.route('/')
 @app.route('/catalogs')
 @app.route('/catalog')
 # list all recent catalog items
 def catalogs():
-    return render_template('catalogsOut.html')
+    # list for nav menu
+    catalogs = session.query(Catalog).all()
+    catalogItems = session.query(CatalogItem).order_by("id asc").limit(10)
+    return render_template('catalogsOut.html', catalogs = catalogs, catalogItems = catalogItems)
 
-# list certain catalog items
-#@app.route('/catalog/')
-#def catalogItems()
+# list all items of certain catalog
+@app.route('/catalog/<catalog_name>/')
+def catalogItems(catalog_name):
+    # list for nav menu
+    catalogs = session.query(Catalog).all()
+    catalog = session.query(Catalog).filter_by(name = catalog_name).one()
+    catalogItems = session.query(CatalogItem).filter_by(catalog_id = catalog.id).all()
+    catalogItemsCount = session.query(CatalogItem).filter_by(catalog_id = catalog.id).count()
+
+    return render_template('catalogItemsOut.html',
+        catalogs = catalogs,
+        catalog = catalog,
+        catalogItems = catalogItems,
+        catalogItemsCount = catalogItemsCount)
+
+# info about specific item
+@app.route('/catalog/<catalog_name>/<catalog_item_name>/')
+def catalogItemInfo(catalog_name, catalog_item_name):
+    # list for nav menu
+    catalogs = session.query(Catalog).all()
+    # get catalog name
+    catalog = session.query(Catalog).filter_by(name = catalog_name).one()
+    # get catalog items
+    catalogItems = session.query(CatalogItem).filter_by(catalog_id = catalog.id).all()
+    # output = ""
+    # output += catalog.name
+    # for item in catalogItems:
+    #   output += item.name
+    #   output += item.description
+    # return output
+    return render_template('catalogItemOut.html', catalogs = catalogs, catalog = catalog, catalogItems = catalogItems)
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
      app.debug = True
