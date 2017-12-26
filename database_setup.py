e474be972a54278d821955db328c93026bd8214c
zach-col <theperfectpicture100@gmail.com>
2017-12-26 16:36:29 +0000

    Create database and home template

---
+++ database_setup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# class for each catalog
+class Catalog(Base):
+    __tablename__ = 'catalog'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class CatalogItem(Base):
+    __tablename__ = 'catalog_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    catalog_id = Column(Integer, ForeignKey('catalog.id'))
+    catalog = relationship(Catalog)
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
